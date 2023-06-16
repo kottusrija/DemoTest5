95bf67b5668a085040747564dfe6b47fdb6dd64e
kottusrija <kottu.srija@capgemini.com>
2023-06-16 09:57:04 +0530

    modified,added new file

--- TestFramework/src/test/java/com/test/webTest/TestJava.java
+++ TestFramework/src/test/java/com/test/webTest/TestJava.java
@@ -3,6 +3,8 @@ package com.test.webTest;
 public class TestJava {
 	public static void main(String[] args) {
 		System.out.println("this is first class for github in the branch");
+		
+		System.out.println("THIS IS NEW LINE");
 	}
 
 }
---
+++ TestFramework/src/test/java/com/test/webTest/TestJava2.java
@@ -0,0 +1,8 @@
+package com.test.webTest;
+
+public class TestJava2 {
+	public static void main(String[] args) {
+		System.out.println("This is second file");
+	}
+
+}
